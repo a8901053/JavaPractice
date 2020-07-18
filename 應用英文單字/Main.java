8f547287bccbcd712d8edcee25b8f832fba8f419
a8901053 <a89010531111@gmail.com>
2020-07-19 00:05:33 +0800

    應用英文單字

---
+++ 應用英文單字/Main.java
@@ -0,0 +1,15 @@
+package 應用英文單字;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            EnglishWordSystem a = new EnglishWordSystem();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
